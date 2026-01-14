244dfcf59fe6c50ca11ec624c1000187daffaf41
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2026-01-14 08:34:35 -0500

    Add terraform that deploys an Intel TDX enabled instance to GCP

---
+++ infra/gcp-tdx/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.14.0, < 2.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.15"
+    }
+  }
+}
+
+provider "google" {
+  project = "bearclave"
+  zone    = "us-central1-a"
+}
+
+variable "service_account_email" {
+  description = "GCP service account email"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for accessing instance"
+  type        = string
+  sensitive   = true
+}
+
+module "gcp_tdx" {
+  source = "git::https://github.com/tahardi/bearclave-tf.git//modules/gcp-tdx?ref=e4b5365fe00637edcaa7886e4ad809fa2b973c56"
+
+  project_id            = "bearclave"
+  service_account_email = var.service_account_email
+  ssh_public_key        = var.ssh_public_key
+  instance_name         = "bearclave-tdx"
+  container_image       = "us-east1-docker.pkg.dev/bearclave/bearclave/hello-https-enclave-tdx@sha256:e9ad9cafc59b6c0291f0b79b78da9132f3fb325d6de0459ce6a7b343abb51bc0"
+}
